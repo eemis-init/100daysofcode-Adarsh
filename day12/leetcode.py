406219611496c6ebaf8cc5858f62a4a973cda2fd
Adarsh Pandey <eemis.crypt@gmail.com>
2022-08-18 15:28:40 +0530

    Update leetcode.py

--- day12/leetcode.py
+++ day12/leetcode.py
@@ -66,3 +66,41 @@ class Solution:
             else:
                 r-=1
         return maxVol
+    
+    #rotate image
+    
+    class Solution:
+    def rotate(self, matrix: List[List[int]]) -> None:
+        """
+        Do not return anything, modify matrix in-place instead.
+        """
+        l, r =0,len(matrix)-1
+        while l<r:
+            for i in range(r-l):
+                top,bottom=l,r
+                topleft=matrix[top][l+i]
+                matrix[top][l+i] = matrix[bottom-i][l]
+                matrix[bottom-i][l] = matrix[bottom][r-i]
+                matrix[bottom][r-i] = matrix[top+i][r]
+                matrix[top+i][r] = topleft
+            
+            l+=1
+            r-=1
+        return matrix
+                
+        #valid parenthesis
+        
+        class Solution:
+    def isValid(self, s: str) -> bool:
+        stack = []
+        pairing = {')':'(',']':'[','}':'{'}
+        
+        for i in s:
+            if i in pairing:        
+                if stack and stack[-1]==pairing[i]:
+                    stack.pop()
+                else:
+                    return False
+            else:
+                stack.append(i)
+        return True if stack==[] else False
