c8bfcaa35d314328ba530881fbcef3c61ec12925
Adarsh Pandey <eemis.crypt@gmail.com>
2022-08-30 00:07:07 +0530

    Add files via upload

---
+++ day23/index.js
@@ -0,0 +1,21 @@
+//pehli baar
+console.log('Hello World');
+
+let name = "Adarsh";
+const interestRate=1;
+//interestRate=0.5;
+console.log(name);
+console.log(interestRate);
+
+let car = 'Ford';
+let age = 7;
+let isApproved = true;
+let firstName = undefined;
+let selectedColor = null;
+
+let person={
+    phone : 'Poco',
+    RAM : 6
+};
+person.phone='Apple';
+person["phone"]="LAWDA";
